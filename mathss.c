cb7073f099416b7878b13a215bbe24577b5e3b5b
Ercan <ercan.yagmur@bil.omu.edu.tr>
2013-11-23 20:50:11 +0200

    Create mathss.c

---
+++ mathss.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i=0;
+    for(i=1;i<=10;i++){
+    if(i%2==0){
+    printf("%d",i);
+    printf("-cift\n");
+}   
+    else{
+         printf("%d",i);
+         printf("-tek\n");
+         }
+}
+  
+  getch();
+  return 0;
+}
